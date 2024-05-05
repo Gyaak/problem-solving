c01b3ed335a190c3370884dbd10303737e3c34a7
gyak <gyak404@gmail.com>
2024-05-06 00:16:11 +0900

    20240506 https://www.acmicpc.net/problem/10993

---
+++ BOJ10993/Main.java
@@ -0,0 +1,68 @@
+package BOJ10993;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    static char[][] board;
+
+    static void draw(int n, int r, int c) {
+        if(n == 1) {
+            board[r][c] = '*';
+            return;
+        }
+        int H = (1<<n)-1;
+        int W = (1<<(n+1))-3;
+        if(n%2==0) {
+            for(int i = 0; i<H; i++) {
+                for(int j = 0; j<W; j++) {
+                    if(i==0 || i==j || i+j==W-1)
+                        board[r+i][c+j] = '*';
+                    else
+                        board[r+i][c+j] = ' ';
+                }
+            }
+            draw(n-1, r+1, c+W/4+1);
+        } else {
+            for(int i = 0; i<H; i++) {
+                for(int j = 0; j<W; j++) {
+                    if(i==H-1 || j-i==W-H || i+j==H-1)
+                        board[r+i][c+j] = '*';
+                    else
+                        board[r+i][c+j] = ' ';
+                }
+            }
+            draw(n-1, r+H/2, c+W/4+1);
+        }
+        
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int H = (1<<n)-1;
+        int W = (1<<(n+1))-3;
+        board = new char[H][W];
+        draw(n, 0, 0);
+        if(n%2==0) {
+            for(int i = 0; i<H; i++) {
+                for(int j = 0; j<W; j++)
+                    if(i+j<=W)
+                        bw.write(board[i][j]);
+                bw.write("\n");    
+            }
+        } else {
+            for(int i = 0; i<H; i++) {
+                for(int j = 0; j<W; j++)
+                    if(j-i<=W-H)
+                        bw.write(board[i][j]);
+                bw.write("\n");    
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
