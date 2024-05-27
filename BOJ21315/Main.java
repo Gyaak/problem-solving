889ed4dd4cbbb4dea3024e63658d930aa8d68d07
gyak <gyak404@gmail.com>
2024-05-28 00:23:50 +0900

    20240528 https://www.acmicpc.net/problem/21315

---
+++ BOJ21315/Main.java
@@ -0,0 +1,58 @@
+package BOJ21315;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] cards;
+
+    static void setInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cards = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++)
+            cards[i] = Integer.parseInt(st.nextToken());
+    }
+
+    static int[] shuffle(int K, int[] arr) {
+        int lastIdx = N-1;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = K; i>=0; i--) {
+            int len = (1<<i);
+            for(int j = lastIdx; j>lastIdx-len; j--)
+                dq.addFirst(arr[j]);
+            for(int j = lastIdx-len; j>=0; j--)
+                arr[j+len] = arr[j];
+            for(int j = 0; j<len; j++)
+                arr[j] = dq.pollFirst();
+            lastIdx = len - 1;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        setInput();
+        for(int i = 1; (1<<i)<N; i++) {
+            for(int j = 1; (1<<j)<N; j++) {
+                int[] tmp = new int[N];
+                for(int k = 0; k<N; k++)
+                    tmp[k] = k+1;
+                shuffle(i, tmp);
+                shuffle(j, tmp);
+                boolean flag = true;
+                for(int k = 0; k<N; k++) {
+                    if(cards[k]!=tmp[k]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) {
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
+    }
+}
