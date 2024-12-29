17363586c4517bafd15acdcf7dfe2b5232c0fb9b
Pak Su Hyung <gyak404@gmail.com>
2024-12-29 21:24:14 +0900

    [Gold V] Title: 줄어드는 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -1,9 +1,15 @@
 # Main Logic
 N = int(input())
-numList = list(range(10))
-result = list(range(10))
-for _ in range(9):
-    numList = [10 * l + i for i in range(10) for l in numList if l % 10 > i]
-    result += numList
-result.sort()
-print(-1 if len(result) < N else result[N - 1])
+if N > 1023:
+    print(-1)
+else:
+    numList = list(range(10))
+    result = list(range(10))
+    for _ in range(10):
+        if len(numList) < N:
+            N -= len(numList)
+        else:
+            numList.sort()
+            print(numList[N - 1])
+            break
+        numList = [10 * l + i for i in range(10) for l in numList if l % 10 > i]
